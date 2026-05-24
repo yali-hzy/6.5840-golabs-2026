d44523ae9d3151a412664f673a0e1f1d003e3a46
yali-hzy <yali-hzy@qq.com>
2026-05-24 17:00:31 +0800

    feat: lock using kv clerk

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,6 +1,7 @@
 package lock
 
 import (
+	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
 )
 
@@ -11,6 +12,8 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	name string
+	id   string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -22,13 +25,64 @@ type Lock struct {
 func MakeLock(ck kvtest.IKVClerk, lockname string) *Lock {
 	lk := &Lock{ck: ck}
 	// You may add code here
+	lk.name = lockname
+	lk.id = kvtest.RandValue(8)
+	lk.ck.Put(lk.name, "unlocked", 0)
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		v, ver, err := lk.ck.Get(lk.name)
+		if err == rpc.ErrNoKey {
+			panic("no such lock " + lk.name)
+		}
+		if err == rpc.OK {
+			switch v {
+			case "unlocked":
+				err = lk.ck.Put(lk.name, lk.id, ver)
+				switch err {
+				case rpc.OK:
+					return
+				case rpc.ErrMaybe:
+					v, ver, err = lk.ck.Get(lk.name)
+					if err == rpc.OK && v == lk.id {
+						return
+					}
+				}
+			case lk.id:
+				panic("lock " + lk.name + " is already held by this client")
+			}
+		}
+	}
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	for {
+		v, ver, err := lk.ck.Get(lk.name)
+		if err == rpc.ErrNoKey {
+			panic("no such lock " + lk.name)
+		}
+		if err == rpc.OK {
+			switch v {
+			case lk.id:
+				err = lk.ck.Put(lk.name, "unlocked", ver)
+				switch err {
+				case rpc.OK:
+					return
+				case rpc.ErrMaybe:
+					v, ver, err = lk.ck.Get(lk.name)
+					if err == rpc.OK && v != lk.id {
+						return
+					}
+				}
+			case "unlocked":
+				panic("lock " + lk.name + " is already unlocked")
+			default:
+				panic("lock " + lk.name + " is held by another client")
+			}
+		}
+	}
 }
